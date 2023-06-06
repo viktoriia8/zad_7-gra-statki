52f08cf2314bbbf74f19ad111a700c35f9fd9373
viktoriia <vikto@LAPTOP-08JQHRKQ>
2023-06-06 16:02:26 +0200

    Zmiany w Board.cpp Metoda ifok do dalszej poprawy

--- zad_7-gra-statki/Board.cpp
+++ zad_7-gra-statki/Board.cpp
@@ -25,6 +25,7 @@ void Board::showmyboard() {
 void Board::addShip(const Ship& s){
 	string place = s.place;
 	int num = atoi(&place[0])-1;
+	if (num == -1)num = 9;
 	int ch = int(place[1])-97;
 	matrix[num][ch] = 1;
 
@@ -44,12 +45,20 @@ bool Board::ifok(const Ship& s) {
 	int edge = 0;
 	string place_ = s.place;
 	int row = atoi(&place_[0])-1;
+	if (row == -1)row = 8;
 	int column = int(place_[1])-97;
-	//int corner=0;
 	if (row == 0 or row == 9)edgen = 1;
 	if (column == 0 or column == 9)edgec = 1;
 	edge = edgen + edgec;
-	//if (edge == 2) corner = 1;
+	int correct=0;
+	for (int g = 0; g < 10; g++) {
+		if (row == g) correct++;
+		if (column == g)correct++;
+	}
+	if (correct != 2) {
+		cout << "le wprowadzone wspó³rzêdne statka!\n";
+		return 0;
+	}
 	if (edge != 2 && edge!=1) {
 		int j = column;
 		int i = row;       //sprawdzenie czy mo¿na umieœciæ statek w czêœci œrodkowej pola(nie z bregów)
@@ -60,7 +69,7 @@ bool Board::ifok(const Ship& s) {
 		}return 1;
 	}
 	if (edgen == 1) {
-		if (s.place[0] == 1) {
+		if (row == 0) {
 			int f = 0;
 			int h = column;
 			for ((h - 1); h<(column + 1); h++) {
@@ -70,7 +79,7 @@ bool Board::ifok(const Ship& s) {
 			return 1;
 		}														//sprawdzanie dla górnego i dolnego brzegu bez kratek w rogu
 
-		if (s.place[0] == 0) {
+		if (row == 9) {
 			int f = 9;
 			int h = column;
 			for ((h - 1); h<(column + 1); h++) {
@@ -82,7 +91,7 @@ bool Board::ifok(const Ship& s) {
 
 	};
 	if (edgec == 1) {
-		if (column == 0) {
+		if (column == 0 and edge != 2) {
 			int f = 0;
 			int h = row;
 			for ((h - 1); h<(row + 1); h++) {
@@ -92,7 +101,7 @@ bool Board::ifok(const Ship& s) {
 			return 1;
 		}														//sprawdzanie dla prawego i lewego brzegu bez kratek w rogu
 
-		if (column == 9) {
+		if (column == 9 and edge!=2) {
 			int f = 9;
 			int h = row;
 			for ((h - 1); h<(row + 1); h++) {
@@ -110,7 +119,7 @@ bool Board::ifok(const Ship& s) {
 			}
 		}return 1;
 	}
-	if (s.place[0] == 0 and s.place[1] == 97) {
+	if (row == 9 and column == 0) {
 		for (int d = 9; d > 7; d--) {
 			for (int k = 0; k < 2; k++) {						//sprawdzenie dla kratki 0a
 				if (matrix[d][k] != 0)return 0;
@@ -118,20 +127,19 @@ bool Board::ifok(const Ship& s) {
 		}return 1;
 	}
 
-	if (row == 0 and column == 9) {
+	if (row == 9 and column == 9) {
 		for (int d = 0; d < 2; d++) {
-			for (int k = 9; k > 7; k--) {                        //sprawdzenie dla kratki 1j
+			for (int k = 9; k > 7; k--) {                        //sprawdzenie dla kratki 0j
 				if (matrix[d][k] != 0)return 0;
 			}
 		}return 1;
 	}
 	if (row == 0 and column == 9) {
 		for (int d = 9; d > 7; d--) {
-			for (int k = 9; k > 7; k--) {						//sprawdzenie dla kratki 0j
+			for (int k = 9; k > 7; k--) {						//sprawdzenie dla kratki 1j
 				if (matrix[d][k] != 0)return 0;
 			}
 		}return 1;
 	}
 
-	return 1;
 };
